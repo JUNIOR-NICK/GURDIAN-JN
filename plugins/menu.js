c328ed06794f580e340b8210f8e5545a72024973
salohcin811 <nkioko676@gmail.com>
2026-07-05 16:32:59 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,1 +1,8 @@
+exports.name = "menu";
+exports.run = async (sock, m, from, args, config) => {
+    let msg = *${config.BOT_NAME} Commands*\n\n;
+    for(const [name] of cmds) msg += ${config.PREFIX}${name}\n;
+    await sock.sendMessage(from, { text: msg });
+};
+
 
