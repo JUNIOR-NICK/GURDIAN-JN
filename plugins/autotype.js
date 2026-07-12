90982dc1d4c80506b4cd4b4607993e965dad5bd1
JUNIOR-NICK <juniortrading54@gmail.com>
2026-07-12 16:12:16 +0300

    Refactor autotype plugin to ping plugin

--- plugins/autotype.js
+++ plugins/autotype.js
@@ -1,6 +1,5 @@
-exports.name = "autotype";
-exports.ownerOnly = true; // <-- add this line
-exports.run = async (sock, m, from, args, config, isOwner) => {
-    config.AUTO_TYPE = !config.AUTO_TYPE;
-    await sock.sendMessage(from, { text: `✅ Auto Typing: ${config.AUTO_TYPE ? 'ON' : 'OFF'}` });
+exports.name = "ping";
+exports.ownerOnly = false; // everyone can use
+exports.run = async (sock, m, from) => {
+    await sock.sendMessage(from, { text: "pong ✅" });
 };
