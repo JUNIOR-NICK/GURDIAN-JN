d3536ef0d6c5521715cada53fba6363ec2202607
JUNIOR-NICK <juniortrading54@gmail.com>
2026-07-12 13:01:04 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,8 +1,23 @@
 exports.name = "menu";
-exports.run = async (sock, m, from, args, config) => {
-    let msg = `*${config.BOT_NAME} Commands*\n\n`;
-    msg += `${config.PREFIX}ping - Check if bot is alive\n`;
-    msg += `${config.PREFIX}menu - Show this menu\n`;
-    msg += `${config.PREFIX}kokote - GUARDIAN iko kokote\n`;
+exports.run = async (sock, m, from, args, config, isOwner) => {
+    let msg = `╭───「 *${config.BOT_NAME}* 」───╮\n`;
+    msg += `│ 🤖 Bot: ${config.BOT_NAME}\n`;
+    msg += `│ 👑 Owner: ${isOwner ? 'You' : 'Hidden'}\n`;
+    msg += `│ 📌 Prefix: ${config.PREFIX}\n`;
+    msg += `╰──────────────────╯\n\n`;
+
+    msg += `*📜 GENERAL COMMANDS*\n`;
+    msg += `├ ${config.PREFIX}ping - Check bot speed\n`;
+    msg += `├ ${config.PREFIX}menu - Show this menu\n`;
+    msg += `└ ${config.PREFIX}kokote - GUARDIAN iko kokote\n\n`;
+
+    if(isOwner){
+        msg += `*👑 OWNER COMMANDS*\n`;
+        msg += `├ ${config.PREFIX}ban - Ban user\n`;
+        msg += `├ ${config.PREFIX}broadcast - Send to all chats\n`;
+        msg += `└ ${config.PREFIX}restart - Restart bot\n`;
+    }
+    
+    msg += `> Powered by GUARDIAN-JN v1.2`;
     await sock.sendMessage(from, { text: msg });
 };
