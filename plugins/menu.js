52c4674da8272625452d36826eaaea25aaf5c4df
JUNIOR-NICK <juniortrading54@gmail.com>
2026-07-12 12:34:05 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,8 +1,8 @@
 exports.name = "menu";
 exports.run = async (sock, m, from, args, config) => {
-    let msg = *${config.BOT_NAME} Commands*\n\n;
-    for(const [name] of cmds) msg += ${config.PREFIX}${name}\n;
+    let msg = `*${config.BOT_NAME} Commands*\n\n`;
+    msg += `${config.PREFIX}ping - Check if bot is alive\n`;
+    msg += `${config.PREFIX}menu - Show this menu\n`;
+    msg += `${config.PREFIX}kokote - GUARDIAN iko kokote\n`;
     await sock.sendMessage(from, { text: msg });
 };
-
-
