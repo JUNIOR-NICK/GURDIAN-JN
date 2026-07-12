958636e991ec7a69045917e2890c1740b8ded992
JUNIOR-NICK <juniortrading54@gmail.com>
2026-07-12 15:34:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
     BOT_NAME: "GUARDIAN-JN",
     PREFIX: ".",
-    AUTO_TYPE: true,      // fake typing/recording
-    AUTO_READ: true,      // blue ticks
-    AUTO_STATUS_LIKE: true // auto view + like status
+    OWNER: "254769532338", // your number no +
+    AUTO_TYPE: true,
+    AUTO_READ: true,
+    AUTO_STATUS_LIKE: true
 }
