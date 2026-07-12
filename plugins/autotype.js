02d92232d56dc831c849836dfc418423ca87ee80
JUNIOR-NICK <juniortrading54@gmail.com>
2026-07-12 15:35:12 +0300

    Update autotype.js

--- plugins/autotype.js
+++ plugins/autotype.js
@@ -1,5 +1,6 @@
 exports.name = "autotype";
-exports.run = async (sock, m, from, args, config) => {
+exports.ownerOnly = true; // <-- add this line
+exports.run = async (sock, m, from, args, config, isOwner) => {
     config.AUTO_TYPE = !config.AUTO_TYPE;
-    await sock.sendMessage(from, { text: `✅ Auto Typing/Recording: ${config.AUTO_TYPE ? 'ON' : 'OFF'}` });
+    await sock.sendMessage(from, { text: `✅ Auto Typing: ${config.AUTO_TYPE ? 'ON' : 'OFF'}` });
 };
