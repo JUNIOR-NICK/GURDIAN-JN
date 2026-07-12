34f1709e8eb30e7b33dde04b78d94c659b999f13
JUNIOR-NICK <juniortrading54@gmail.com>
2026-07-12 16:27:46 +0300

    Update bot owner number in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     BOT_NAME: "GUARDIAN-JN",
     PREFIX: ".",
-    OWNER: "254769532338", // your number no +
+    OWNER: "254112843071", // your number no +
     AUTO_TYPE: true,
     AUTO_READ: true,
     AUTO_STATUS_LIKE: true
